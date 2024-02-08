a5c03f06eafd82525231677e8b78f931fa51e604
kunek29 <andrzej.kunecki@gmail.com>
2024-02-08 22:31:16 +0100

    Fix graphic layout

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ModelMultipleChoiceField
+from django.forms import ModelForm, Textarea, DecimalField
 from auctions.models import Listing, User, Bids, Categories, Comments, Watchlist
 from django import forms
 
@@ -8,12 +8,22 @@ class ListingForm(ModelForm):
     class Meta:
         model = Listing
         fields = ["title", "description", "image_url", "min_bid", "category"]
+        labels = {
+            "image_url": "Add image Url (optional)",
+            "min_bid": "Set min. bid"}
 
 
 class CommentsForm(ModelForm):
+    #comment = forms.CharField(widget=Textarea, label='')
     class Meta:
         model = Comments
         fields = ["comment"]
+        widgets = {"comment": Textarea(attrs={
+            "cols": 60, 
+            "rows": 2,
+            "placeholder": "Comment here"})}
+        labels = {"comment": ''}
+
 
 
 class BidsForm(ModelForm):
